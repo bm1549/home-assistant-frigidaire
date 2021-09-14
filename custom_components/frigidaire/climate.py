f6ea569cfbbb35feac42abdadca11e018a17c56b
Chase Sterling <chase.sterling@gmail.com>
2021-09-14 12:00:19 -0400

    Err on the side of connected

--- custom_components/frigidaire/climate.py
+++ custom_components/frigidaire/climate.py
@@ -290,5 +290,5 @@ class FrigidaireClimate(ClimateEntity):
                 self._details.for_code(
                     frigidaire.HaclCode.CONNECTIVITY_STATE
                 ).string_value
-                == frigidaire.ConnectivityState.CONNECTED.value
+                != frigidaire.ConnectivityState.DISCONNECTED.value
             )
