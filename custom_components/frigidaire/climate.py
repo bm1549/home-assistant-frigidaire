5dcd4e5b8e7dcc12b17bd591816b32d8f5306c8d
Brian Murray <brian@murraytwins.com>
2026-07-27 03:35:25 -0700

    Fix hvac_mode not reflecting OFF when appliance is powered off via ap… (#143)
    Co-authored-by: Brian Marks <bmarks1056@gmail.com>

--- custom_components/frigidaire/climate.py
+++ custom_components/frigidaire/climate.py
@@ -249,6 +249,9 @@ class FrigidaireClimate(CoordinatorEntity[FrigidaireApplianceCoordinator], Clima
         """Return current operation i.e. heat, cool, idle."""
         if self._is_optimistic() and self._optimistic_hvac_mode is not None:
             return self._optimistic_hvac_mode
+        appliance_state = _normalize_enum_value(self._details.get(frigidaire.Detail.APPLIANCE_STATE))
+        if appliance_state == frigidaire.ApplianceState.OFF:
+            return HVACMode.OFF
         frigidaire_mode = _normalize_enum_value(self._details.get(frigidaire.Detail.MODE))
 
         if frigidaire_mode not in FRIGIDAIRE_TO_HA_MODE:
@@ -393,7 +396,9 @@ class FrigidaireClimate(CoordinatorEntity[FrigidaireApplianceCoordinator], Clima
         else:
             if hvac_mode not in HA_TO_FRIGIDAIRE_HVAC_MODE:
                 return
-            if _normalize_enum_value(self._details.get(frigidaire.Detail.MODE)) == frigidaire.Mode.OFF:
+            appliance_state = _normalize_enum_value(self._details.get(frigidaire.Detail.APPLIANCE_STATE))
+            frigidaire_mode = _normalize_enum_value(self._details.get(frigidaire.Detail.MODE))
+            if appliance_state == frigidaire.ApplianceState.OFF or frigidaire_mode == frigidaire.Mode.OFF:
                 self._client.execute_action(self._appliance, frigidaire.Action.set_power(frigidaire.Power.ON))
                 # temperature reverts to default when the device is turned on
                 current_temp = self.target_temperature
